2ed625aa60a1ce2c7f175d950f5eca27c41fca8d
agalloch7 <yixuanli@g.harvard.edu>
2014-10-23 22:20:04 -0400

    Created a table for Random User Generator

--- app/views/fakers-results.blade.php
+++ app/views/fakers-results.blade.php
@@ -8,22 +8,33 @@
 	    <h3 class="panel-title">Here's your random users</h3>
 	  </div>
 	  <div align="left" class="panel-body">
+	  	<table class="table table-striped">
+	  		<tr>
+	  			<td><h3>Name</h3></td>
+	  			<td><h3>Email</h3></td>
+	  			<td><h3>Address</h3></td>
+	  			<td><h3>Phone Number</h3></td>
+	  			<td><h3>Credit Card Type</h3></td>
+	  			<td><h3>Credit Card Number</h3></td>
+	  			<td><h3>Credit Card Expiration Date</h3></td>
 		<?php
 		$faker = Faker\Factory::create();
 			for ($i = 0; $i < $users; $i++)
-			{
-				echo $faker->name . '<br />';
-				echo $faker->userName . '<br />';
-			        echo $faker->email . '<br />';
-			        echo $faker->address . '<br />';
-			        echo $faker->phoneNumber . '<br />';
-			        echo $faker->creditCardType . '<br />';
-			        echo $faker->creditCardNumber . '<br />';
-			        echo $faker->creditCardExpirationDateString . '<br />';
-			        
-
+			{ 
+				?>
+				<tr> 
+				<td> <?php echo $faker->name; ?> </td>
+			        <td> <?php echo $faker->email; ?> </td>
+			        <td> <?php echo $faker->address; ?> </td>
+			        <td> <?php echo $faker->phoneNumber; ?> </td>
+			        <td> <?php echo $faker->creditCardType; ?> </td>
+			        <td> <?php echo $faker->creditCardNumber; ?> </td>
+			        <td> <?php echo $faker->creditCardExpirationDateString; ?> </td>
+			    </tr>
+		<?php
 			}
 		?>
+	</table>
 	  </div>
 	</div>
 </div>
